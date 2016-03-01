b06bd67dc9e1b95be0dea3650eac6ef019ddc34d
yiyang <yiyang.bao@gmail.com>
2016-03-01 14:27:26 +0000

    #1 new game view UI

--- app/src/main/java/com/csbob/mahjongdiary/GameListActivity.java
+++ app/src/main/java/com/csbob/mahjongdiary/GameListActivity.java
@@ -1,8 +1,8 @@
 package com.csbob.mahjongdiary;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -30,12 +30,11 @@ public class GameListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                createNewGame(view);
             }
         });
 
-        final ListView listView = (ListView) findViewById(R.id.gameList);
+        final ListView listView = (ListView) findViewById(R.id.game_list);
         final ArrayAdapter<String> adaptor = new GameListAdaptor(this, data);
         listView.setAdapter(adaptor);
 
@@ -79,5 +78,10 @@ public class GameListActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createNewGame(View view) {
+        Intent intent = new Intent(this, NewGameActivity.class);
+        startActivity(intent);
     }
 }
